3336c92ccabbe82a4629507afb18f7ff4b6184ca
Brian Bansenauer <brianb@cascadia.edu>
2019-04-01 22:06:51 -0700

    extracted quiz

---
+++ Quiz/Quiz.swift
@@ -0,0 +1,34 @@
+//
+//  Quiz.swift
+//  Quiz
+//
+//  Created by Brian Bansenauer on 4/1/19.
+//  modified from iOS Programming 6ed, Big Nerd Ranch
+//
+
+class Quiz{
+ 
+private let questions: [String] = [
+    "From what is cognac made?",
+    "What is 7+7?",
+    "What is the capital of Vermont?"
+]
+    
+private let answers: [String] = [
+    "Grapes",
+    "14",
+    "Montpelier"
+]
+
+    var count : Int {
+        return questions.count
+    }
+    
+    func question( number num:Int ) -> String {
+        return questions[num]
+    }
+    
+    func answer( number num:Int ) -> String {
+        return answers[num]
+    }
+}
